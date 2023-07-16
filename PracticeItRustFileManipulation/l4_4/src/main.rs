ce15248300c0c92dc25f91311bf52cccfce9bc74
YasinVeliyev <veliyev.yasin@gmail.com>
2023-07-16 19:10:03 +0400

    new file:   PracticeItRustFileManipulation/l4_4/Cargo.lock 	new file:   PracticeItRustFileManipulation/l4_4/Cargo.toml 	new file:   PracticeItRustFileManipulation/l4_4/src/main.rs

---
+++ PracticeItRustFileManipulation/l4_4/src/main.rs
@@ -0,0 +1,30 @@
+use std::collections::HashMap;
+
+fn main() {
+    let contents = String::from("This is the first line\nThe second line is a little longer\nLine 3 is short\nThe 4th line is the first non-prime\nThe 5th line has the starting five");
+    let words = contents
+        .split_whitespace()
+        .map(|word| word.to_owned())
+        .collect();
+    let replacement_map = HashMap::from([
+        ("first".to_string(), "last".to_string()),
+        ("line".to_string(), "entry".to_string()),
+    ]);
+
+    println!("Hello, world!");
+    println!("{:?}", replace_x_with_y(words, replacement_map));
+}
+
+fn replace_x_with_y(
+    contents: Vec<String>,
+    replacement_map: HashMap<String, String>,
+) -> Vec<String> {
+    let mut contents = contents;
+    contents
+        .iter_mut()
+        .for_each(|word| match replacement_map.get(word) {
+            Some(value) => *word = value.clone(),
+            None => {}
+        });
+    contents
+}
