315ec8da1fced2d05983692834b40ea937bf053e
YasinVeliyev <veliyev.yasin@gmail.com>
2023-07-16 17:26:11 +0400

    new file:   PracticeItRustFileManipulation/l3_10/Cargo.lock 	new file:   PracticeItRustFileManipulation/l3_10/Cargo.toml 	new file:   PracticeItRustFileManipulation/l3_10/file_with_lines 	new file:   PracticeItRustFileManipulation/l3_10/src/main.rs

---
+++ PracticeItRustFileManipulation/l3_10/src/main.rs
@@ -0,0 +1,18 @@
+use std::io::Error;
+
+fn main() {
+    println!("Hello, world!");
+    println!("{:?}", read_file("file_with_lines").unwrap())
+}
+
+fn read_file(path: &str) -> Result<Vec<Vec<String>>, Error> {
+    Ok(std::fs::read_to_string(path)
+        .unwrap()
+        .lines()
+        .map(|line| {
+            line.split_whitespace()
+                .map(|word| word.to_owned())
+                .collect()
+        })
+        .collect::<Vec<Vec<String>>>())
+}
