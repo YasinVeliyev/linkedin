16f5ef320256265819ca552d98d497dffe740607
YasinVeliyev <veliyev.yasin@gmail.com>
2023-07-16 20:18:53 +0400

    new file:   PracticeItRustFileManipulation/CertificateOfCompletion_Practice It Rust File Manipulation.pdf 	deleted:    PracticeItRustFileManipulation/l5_2/marcus_chapter_1 	modified:   PracticeItRustFileManipulation/l5_2/src/main.rs

--- PracticeItRustFileManipulation/l5_2/src/main.rs
+++ PracticeItRustFileManipulation/l5_2/src/main.rs
@@ -25,9 +25,17 @@ fn main() {
         ("girl".to_string(), "boy".to_string()),
     ]);
     let mut contents = read_file("alice_chapter_1");
-    let _ = contents
+
+    let c = &contents
         .iter()
-        .map(|words| replace_x_with_y(words.clone(), &replacement_map));
+        .map(|words| words.join(" "))
+        .collect::<Vec<String>>();
+
+    println!("{:#?}", count_words(c.join(" ")));
+
+    let _ = contents
+        .iter_mut()
+        .for_each(|words| replace_x_with_y(words, &replacement_map));
 
     write_file("marcus_chapter_1.txt", contents).unwrap();
 }
@@ -45,8 +53,7 @@ fn read_file(path: &str) -> Vec<Vec<String>> {
         .collect()
 }
 
-fn replace_x_with_y(contents: Vec<String>, replacement_map: &HashMap<String, String>) {
-    let mut contents = contents;
+fn replace_x_with_y(contents: &mut Vec<String>, replacement_map: &HashMap<String, String>) {
     contents.iter_mut().for_each(|word| {
         if let Some(value) = replacement_map.get(word) {
             *word = value.clone()
@@ -60,4 +67,14 @@ fn write_file(path: &str, contents: Vec<Vec<String>>) -> Result<(), Error> {
         .map(|words| words.join(" "))
         .collect::<Vec<String>>();
     fs::write(path, contents.join("\n"))
+}
+
+fn count_words(contents: String) -> HashMap<String, u32> {
+    let mut map = HashMap::new();
+    contents.split_whitespace().for_each(|word| {
+        map.entry(word.trim().to_lowercase())
+            .and_modify(|v| *v += 1)
+            .or_insert(1);
+    });
+    map
 }
