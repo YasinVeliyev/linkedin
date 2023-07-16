b25ce1804bed04caf364b60789ac4ebba6932827
YasinVeliyev <veliyev.yasin@gmail.com>
2023-07-16 15:32:22 +0400

    modified:   PracticeItRustFileManipulation/l1_3/src/main.rs

--- PracticeItRustFileManipulation/l1_3/src/main.rs
+++ PracticeItRustFileManipulation/l1_3/src/main.rs
@@ -1,5 +1,8 @@
 use std::fs;
 
 fn main() {
-    println!("{}", fs::read_to_string("Cargo.toml").unwrap());
+    println!(
+        "{}",
+        fs::read_to_string("Cargo.toml").expect("Unable to read the file")
+    );
 }
