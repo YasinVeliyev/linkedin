e8565f98a176ab27ed7672bf40b2ab17e8c54bbb
YasinVeliyev <veliyev.yasin@gmail.com>
2023-07-25 00:22:51 +0400

    modified:   LevelUpRust/tasks/src/handle_inconsistent_dates.rs 	modified:   LevelUpRust/tasks/src/main.rs 	modified:   LevelUpRust/tasks/tests/handle_inconsistent_dates.rs

--- LevelUpRust/tasks/src/handle_inconsistent_dates.rs
+++ LevelUpRust/tasks/src/handle_inconsistent_dates.rs
@@ -1,17 +1,25 @@
 use chrono::NaiveDate;
 
 pub fn flexible_date_parse(text: &str) -> Option<NaiveDate> {
-    let fmts = vec!["%D","%x","%F","%v","%Y-%m-%d","%Y/%b/%d","%d.%b.%Y","%b.%d.%Y","%Y/%B%/%d","%d.%B.%Y","%b.%B.%Y"];
-    for fmt in fmts{
-         if  let Ok(d) = NaiveDate::parse_from_str(text, fmt){
-            return Some(d)
-        }
-        else{
+    let fmts = vec![
+        "%D",
+        "%x",
+        "%F",
+        "%v",
+        "%Y-%m-%d",
+        "%Y/%b/%d",
+        "%d.%b.%Y",
+        "%b.%d.%Y",
+        "%Y/%B%/%d",
+        "%d.%B.%Y",
+        "%b.%B.%Y",
+    ];
+    for fmt in fmts {
+        if let Ok(d) = NaiveDate::parse_from_str(text, fmt) {
+            return Some(d);
+        } else {
             continue;
         }
     }
     None
-    
 }
-
-
--- LevelUpRust/tasks/src/main.rs
+++ LevelUpRust/tasks/src/main.rs
@@ -1,5 +1,7 @@
 use chrono::NaiveDate;
-use tasks::{run_length_encoding::RunLengthEncoding, handle_inconsistent_dates::flexible_date_parse};
+use tasks::{
+    handle_inconsistent_dates::flexible_date_parse, run_length_encoding::RunLengthEncoding,
+};
 
 fn main() {
     let decoded_string = "AAAAA AAAAAAAAAA AAAAAAAAAAAAAAAAAAAA";
@@ -11,6 +13,6 @@ fn main() {
     );
     println!("{}", encoded_string);
 
-    println!("{:?}",NaiveDate::parse_from_str("1999/Mar/02", "%Y/%b/%d"));
-    println!("{:?}",flexible_date_parse("1999/Mar/02"))
+    println!("{:?}", NaiveDate::parse_from_str("1999/Mar/02", "%Y/%b/%d"));
+    println!("{:?}", flexible_date_parse("1999/Mar/02"))
 }
